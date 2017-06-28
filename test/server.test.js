db0198f5de332f4504d74b81636e061cefb54647
lonord <lonord@qq.com>
2017-06-28 21:06:48 +0800

    add some test cases

--- test/server.test.js
+++ test/server.test.js
@@ -2,8 +2,10 @@ const assert = require('assert')
 const request = require('supertest')
 const path = require('path')
 const rimraf = require('rimraf-then')
+const express = require('express')
 const server = require('../lib/server')
 const builder = require('../lib/builder')
+const index = require('../lib/index')
 
 describe('test server', () => {
 
@@ -36,6 +38,41 @@ describe('test server', () => {
 		let middleWare = await builder.getProdMiddleware(config, cwd)
 		assert.ok(!!middleWare)
 		let app = server.createServer(middleWare).getApp()
+
+		await request(app).get('/').expect(200).expect('Content-Type', 'text/html')
+		await request(app).get('/bundle.js').expect(200)
+	})
+
+	it('custom server in development mode should run success', async () => {
+		let cwd = path.join(__dirname, 'test.builder.env1')
+		let reeasy = index({
+			cwd: cwd,
+			dev: true
+		})
+		let middleware = await reeasy.prepare()
+		let app = express()
+		app.use(middleware)
+
+		await request(app).get('/').expect(200).expect('Content-Type', 'text/html')
+		await request(app).get('/bundle.js').expect(200)
+		
+		await middleware.closeWebpack()
+	})
+
+	it('custom server in production mode should run success', async () => {
+		let cwd = path.join(__dirname, 'test.builder.env1')
+		let config = await builder.readConfig(null, cwd)
+		assert.ok(!!config)
+		let webpackConfig = await builder.prepareWebpack(config, false, cwd)
+		await builder.build(webpackConfig)
+		
+		let reeasy = index({
+			cwd: cwd,
+			dev: false
+		})
+		let middleware = await reeasy.prepare()
+		let app = express()
+		app.use(middleware)
 
 		await request(app).get('/').expect(200).expect('Content-Type', 'text/html')
 		await request(app).get('/bundle.js').expect(200)
