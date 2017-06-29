417dcb49bc35b819a975b03bed18b2babcc2b67f
lonord <lonord@qq.com>
2017-06-29 14:05:44 +0800

    add d.ts file

---
+++ index.d.ts
@@ -0,0 +1,33 @@
+interface ReeasyConfig {
+	/**
+	 * Current work directory, use `process.cwd()` by default
+	 */
+	cwd: string
+	/**
+	 * Is in development mode, default `false`
+	 */
+	dev: boolean
+	/**
+	 * Custom `reeasy.config.js` file path when in format of string;  
+	 * Object exported in `reeasy.config.js` file when in format of object
+	 */
+	conf: string | object
+}
+
+interface ReeasyMiddleware {
+}
+
+interface Reeasy {
+	/**
+	 * Prepare middleware
+	 */
+	prepare(): Promise<ReeasyMiddleware>
+}
+
+declare module "reeasy" {
+	/**
+	 * Create Reeasy instance
+	 * @param opts 
+	 */
+	function create(opts: ReeasyConfig): Reeasy
+}
